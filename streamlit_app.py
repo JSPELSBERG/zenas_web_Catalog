f59f079ae073d7ccbfd8b370832ea45b14be90f8
JSPELSBERG <j.spelsberg@saracus.com>
2024-06-14 12:09:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,6 @@ from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title("Zenas's Amazing Athleisure Catalog")
 
-
-
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
@@ -23,4 +21,16 @@ option = st.selectbox('Pick a sweatsuite color or style:', list(color_list))
 product_caption = 'Our warm, comforable, ' + option + 'sweatsuit!'
 st.write(product_caption)
 
-my_data_row = my_cur.fetchone()
+#my_data_row = my_cur.fetchone()
+
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
+color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+st.image(
+df2[0],
+width=400,
+caption= product_caption
+)
+st.write('Price: ', df2[1])
+st.write('Sizes Available: ',df2[2])
+st.write(df2[3])
