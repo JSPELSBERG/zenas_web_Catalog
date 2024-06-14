c1011cef0cc373f6f1a2986bac08b7eee7a61f0c
JSPELSBERG <j.spelsberg@saracus.com>
2024-06-14 11:33:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import requests
+import pandas as pd
+from snowflake.snowpark.functions import col
 
+# Write directly to the app
+st.title("Zenas's Amazing Athleisure Catalog")
+st.write(
+    """Pick a sweatsuite color or style:"""
+)
